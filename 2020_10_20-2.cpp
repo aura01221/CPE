87bad06bf525acd36a5c6ab34ead10be92e66eac
aura <aura01221@gmail.com>
2021-10-13 23:10:10 +0800

    2020/10/13

---
+++ 2020_10_20-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string input;
+    while (cin >> input)
+    {
+        for (int i = 0; i < input.size(); i++)
+        {
+            cout << char(input[i] - 7);
+        }
+        cout << endl;
+    }
+}
